d49e429d4305815190f82b4e4bce5f5b01ed0e71
Priyantha <IT21021534@my.sliit.lk>
2022-09-27 17:01:36 +0530

    Register Form Fragments

---
+++ app/src/main/java/com/example/foodapp/RegisterMain.java
@@ -0,0 +1,63 @@
+package com.example.foodapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class RegisterMain extends AppCompatActivity {
+
+    Spinner regTypeSpinner;
+
+    CustomerRegisterFragment cusFragment;
+    RestaurantRegisterFragment resFragment;
+    DriverRegisterFragment driFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_main);
+
+
+        Spinner regTypeSpinner = (Spinner) findViewById(R.id.regTypeSpinner);
+        cusFragment = new CustomerRegisterFragment();
+        resFragment = new RestaurantRegisterFragment();
+        driFragment = new DriverRegisterFragment();
+
+        //spinner (dropdown) to select Register Form
+        ArrayAdapter<String> regTypeAdapter = new ArrayAdapter<>(RegisterMain.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.regTypes));
+        regTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        regTypeSpinner.setAdapter(regTypeAdapter);
+
+        regTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                switch(i){
+                    case 0: setFragment(cusFragment);
+                        break;
+                    case 1: setFragment(resFragment);
+                        break;
+                    case 2: setFragment(driFragment);
+                        break;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+    }
+
+    public void setFragment(Fragment fragment){
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.RegFormFragmentView, fragment);
+        fragmentTransaction.commit();
+    }
+}
